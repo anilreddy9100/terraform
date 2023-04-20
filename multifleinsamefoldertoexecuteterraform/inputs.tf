35cf13247fc7b17ae1e47929ffaaecab9b14bd85
Your Name <fgfdg@gmail.com>
2023-04-20 11:15:36 +0530

    added varible using object method

--- multifleinsamefoldertoexecuteterraform/inputs.tf
+++ multifleinsamefoldertoexecuteterraform/inputs.tf
@@ -3,22 +3,36 @@ variable "region" {
   default     = "ap-south-1"
   description = "ask to user to set region were they need resource "
 }
-variable "my-vpc-range" {
-  type        = string
-  default     = "192.168.0.0/16"
-  description = "ask user to set specfic range to create vpc"
-}
+//variable "my-vpc-range" {
+//  type        = string
+//  default     = "192.168.0.0/16"
+//  description = "ask user to set specfic range to create vpc"
+//}
 
-variable "myAZ" {
-  type        = list(string)
-  default     = ["a", "b", "a", "b"]
-  description = "ask user to use specfir AZ"
+// variable "myAZ" {
+//  type        = list(string)
+//  default     = ["a", "b", "a", "b"]
+//  description = "ask user to use specfir AZ"
 
-}
+//}
 
-variable "tagname" {
-  type        = list(string)
-  default     = ["app1", "db1", "app2", "db2"]
-  description = "ask user to give env names"
+# variable "tagname" {
+# type        = list(string)
+# default     = ["app1", "db1", "app2", "db2"]
+#description = "ask user to give env names"
 
+//}
+
+variable "vpc-creaction-info" {
+  type = object({
+    vpc_cidr= string
+    subnet_azs= list(string)
+    subnet_names = list(string) 
+  })
+  default = {
+    subnet_azs = [ "a","b","a","b" ]
+    subnet_names = [ "app1","app2","db1","db2" ]
+    vpc_cidr = "192.168.0.0/16"
+  }
+  
 }
--- multifleinsamefoldertoexecuteterraform/main.tf
+++ multifleinsamefoldertoexecuteterraform/main.tf
@@ -1,17 +1,17 @@
 resource "aws_vpc" "myvpc" {
-  cidr_block = var.my-vpc-range
+  cidr_block = var.vpc-creaction-info.vpc_cidr
   tags = {
     Name = "myvpc"
   }
 }
 
 resource "aws_subnet" "subnets" {
-  count             = length(var.tagname)
-  cidr_block        = cidrsubnet(var.my-vpc-range, 8, count.index)
-  availability_zone = "${var.region}${var.myAZ[count.index]}"
+  count             = length(var.vpc-creaction-info.subnet_names)
+  cidr_block        = cidrsubnet(var.vpc-creaction-info.vpc_cidr, 8, count.index)
+  availability_zone = "${var.region}${var.vpc-creaction-info.subnet_azs[count.index]}"
   vpc_id            = aws_vpc.myvpc.id # implicit dependency
   tags = {
-    Name = var.tagname[count.index]
+    Name = var.vpc-creaction-info.subnet_names[count.index]
   }
   depends_on = [
     aws_vpc.myvpc
